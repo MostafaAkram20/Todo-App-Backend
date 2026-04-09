b72a1ed48e4e17594eb72d04324322b646239250
MostafaAkram20 <mostafaakrm7@gmail.com>
2026-04-09 16:55:22 +0200

    refactor:Error:404-page not found

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -2,13 +2,17 @@ import DBConnection from "../DB/connection/connection.js"
 import usersRoutes from '../routes/user.routes.js'
 import todosRoutes from '../routes/todos.routes.js'
 const bootstrap = (app, express) => {
-    
+
     app.get('/', (req, res) => res.json({ message: 'Localhost' }))
+    app.use('/users', usersRoutes)
+    app.use('/todos', todosRoutes)
 
-    app.use('/users' , usersRoutes)
-    app.use('/todos' , todosRoutes)
+    app.use((req, res) => { // page not found
+        res.status(404).json({
+            message: 'Error 404 Page Not Found'
+        });
+    });
 
-    
     DBConnection()
 }
 
