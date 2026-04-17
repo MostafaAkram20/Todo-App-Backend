40cfbcfa63701a7193079a45bca1cb3ed70733ee
MostafaAkram20 <mostafaakrm7@gmail.com>
2026-04-18 01:24:33 +0200

    fix password bugs

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose"
 import userModel from "../DB/models/user.model.js"
 import bcrypt from 'bcrypt'
-// import validator from 'validator';
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -10,13 +9,13 @@ export const getAllUsers = async (req, res) => {
         return res.status(200).json({ message: 'List of all users', data: users })
     } catch (error) {
         return res.status(500).json({ message: error.message })
-    } 
+    }
 
 }
 export const signUp = async (req, res) => {
 
     try {
-        let { firstName, lastName, userName, password , dateOfBirth } = req.body
+        let { firstName, lastName, userName, password, dateOfBirth } = req.body
         // console.log(firstName , lastName , userName , password);
 
         if (!firstName || !lastName || !userName || !password) {
@@ -33,10 +32,10 @@ export const signUp = async (req, res) => {
                     firstName: firstName,
                     lastName: lastName,
                     password: hashedPassword,
-                    dateOfBirth:dateOfBirth
+                    dateOfBirth: dateOfBirth
                 })
                 let user = await userModel.find().select('userName firstName lastName role dateOfBirth')
-                return res.status(201).json({ message: 'Account is created successfully' , user })
+                return res.status(201).json({ message: 'Account is created successfully', user })
             }
         }
     } catch (error) {
@@ -49,19 +48,19 @@ export const signIn = async (req, res) => {
         let { userName, password } = req.body
         // console.log(firstName , lastName , userName , password);
 
-        if ( !userName || !password) {
+        if (!userName || !password) {
             return res.status(400).json({ message: 'please fill all required fields' })
         } else {
-            const isExistUser = await userModel.findOne({ userName })
-            if (!isExistUser) {
+            const user = await userModel.findOne({ userName })
+            if (!user) {
                 return res.status(401).json({ message: 'user is not exist' })
             } else {
-                const isComapared = bcrypt.compareSync(password , isExistUser.password)
-                if(!isComapared){
-                    return res.status(400).json({message:'Username or password is incorrect'})
-                }else{ // payload and secret key
+                const isComapared = bcrypt.compareSync(password, user.password)
+                if (!isComapared) {
+                    return res.status(400).json({ message: 'Username or password is incorrect' })
+                } else { // payload and secret key
                     //adding email to token
-                    const token = jwt.sign({id:isExistUser._id ,userName:isExistUser.userName, isLoggedIn:true} , process.env.JWT_SECRET , {expiresIn:'1d'})
+                    const token = jwt.sign({ id: user._id, role: user.role, userName: user.userName, isLoggedIn: true }, process.env.JWT_SECRET, { expiresIn: '1d' })
                     return res.status(200).json({ message: 'user logged in successfully', token })
                 }
             }
@@ -92,31 +91,26 @@ export const deleteUser = async (req, res) => {
     }
 }
 
-export const updateUser = async(req , res)=>{
+export const updateUser = async (req, res) => {
     try {
         let { id } = req.params
         // console.log(id);
         if (!mongoose.Types.ObjectId.isValid(id) || !id) {
             return res.status(409).json({ message: 'Error, invalid Id' })
         } else {
-            const user = await userModel.findByIdAndUpdate(id , req.body , {returnDocument: 'after' , runValidators:true})
-            // return res.status(200).json({user:user})
+            const user = await userModel.findByIdAndUpdate(id, req.body, { returnDocument: 'after', runValidators: true })
             if (!user) {
                 return res.status(400).json({ message: 'Error, User not found!' })
             } else {
-                if(req.body.password){
+                if (req.body.password) {
                     const hashedPassword = bcrypt.hashSync(req.body.password, 8)
                     user.password = hashedPassword
                 }
-                return res.status(200).json({ message: 'User updated successfully' , user:user })
+                return res.status(200).json({ message: 'User updated successfully', user: user })
             }
         }
 
     } catch (error) {
         return res.status(500).json({ message: 'Error accured while updating the user', msg: error.message })
     }
-}
-
-export const userProfile = async(req , res)=>{
-
 }
